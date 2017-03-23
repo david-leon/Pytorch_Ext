693c1c429423cb45231f4f5864ab5f04ed5642c5
DavidLeon <daweileng@live.com>
2017-03-23 17:07:07 +0800

    CTC_Log test passed, ready for release

--- pytorch_ext/nn/functional.py
+++ pytorch_ext/nn/functional.py
@@ -1,5 +1,5 @@
 """Functional interface"""
-
+import numpy as np
 import torch
 from torch.autograd import Variable
 
@@ -12,7 +12,7 @@ class CTC_Log(object):
     C: class number
     T: time length (maximum time length of a batch)
     """
-    def __init__(self, eps=1E-12, inf=1E12, align='pre'):
+    def __init__(self, eps=1E-12, inf=1E12, align='pre', device=-1):
         """
         :param eps:
         :param inf:
@@ -22,6 +22,7 @@ class CTC_Log(object):
         self.eps = eps
         self.inf = inf
         self.align = align
+        self.device = device     # < 0 means cpu, >=0 means gpu
 
     def cost(self, queryseq, scorematrix, queryseq_mask=None, scorematrix_mask=None, blank_symbol=None):
         """
@@ -57,17 +58,25 @@ class CTC_Log(object):
             blank_symbol = scorematrix.size(2) - 1
         if queryseq_mask_padded is None:
             queryseq_mask_padded = Variable(torch.ones(queryseq_padded.size()).byte())
+            if self.device >= 0:
+                queryseq_mask_padded = queryseq_mask_padded.cuda(self.device)
         if scorematrix_mask is None:
             scorematrix_mask = Variable(torch.ones(scorematrix.size(0), scorematrix.size(1)).byte())
+            if self.device >= 0:
+                scorematrix_mask = scorematrix_mask.cuda(self.device)
 
         pred_y   = self._class_batch_to_labeling_batch(queryseq_padded, scorematrix, scorematrix_mask)  # (B, T, 2L+1), reshaped scorematrix
         r2, r3   = self._recurrence_relation(queryseq_padded, queryseq_mask_padded, blank_symbol)  # r2 (2L+1, 2L+1), r3 (2L+1, 2L+1, B)
         pred_y   = self._epslog(pred_y)
         B, T, L2 = pred_y.size()
         p_prev   = Variable(self._epslog(torch.eye(1, L2).expand(B,L2) * torch.ones(B, L2)))    # (B, 2L+1)
+        if self.device >= 0:
+            p_prev = p_prev.cuda(self.device)
         queryseq_mask_padded = queryseq_mask_padded.float()
         if self.align == 'pre':
             alphas = Variable(torch.Tensor(T, B, L2))    # (T, B, 2L+1)
+            if self.device >= 0:
+                alphas = alphas.cuda(self.device)
         for i in range(T):
             p_curr = pred_y[:, i, :]                                 # (B, 2L+1)
             p2     = self._log_dot_matrix(p_prev, r2)      # (B, 2L+1)
@@ -109,9 +118,7 @@ class CTC_Log(object):
         zeros_to_minus_inf = (z.max(0)[0] - 1).squeeze().t() * self.inf                        # (B, 2L+1)
         return log_dot + zeros_to_minus_inf
 
-
-    @staticmethod
-    def _pad_blanks(queryseq, blank_symbol, queryseq_mask=None):
+    def _pad_blanks(self, queryseq, blank_symbol, queryseq_mask=None):
         """
         Pad queryseq and corresponding queryseq_mask with blank symbol
         :param queryseq  (L, B)      -> (B, L)    LongTensor
@@ -122,9 +129,13 @@ class CTC_Log(object):
         # for queryseq
         queryseq_extended = queryseq.unsqueeze(2)                                        # (B, L) -> (B, L, 1)
         blanks            = Variable(torch.zeros(queryseq_extended.size()).long() + blank_symbol)         # (B, L, 1)
+        if self.device >= 0:
+            blanks = blanks.cuda(self.device)
         concat            = torch.cat((queryseq_extended, blanks), 2)                    # concat.shape = (B, L, 2)
         res               = concat.view(concat.size(0), concat.size(1) * concat.size(2)) # (B, 2L), the reshape will cause the last 2 dimensions interlace
         begining_blanks   = Variable(torch.zeros((res.size(0), 1)).long() + blank_symbol)                 # (B, 1)
+        if self.device >= 0:
+            begining_blanks = begining_blanks.cuda(self.device)
         queryseq_padded   = torch.cat((begining_blanks, res), 1)                         # (B, 2L+1)
 
         # for queryseq_mask
@@ -133,13 +144,14 @@ class CTC_Log(object):
             concat                 = torch.cat((queryseq_mask_extended, queryseq_mask_extended), 2)  # concat.shape = (B, L, 2)
             res                    = concat.view(concat.size(0), concat.size(1) * concat.size(2))
             begining_blanks        = Variable(torch.ones((res.size(0), 1)).byte())
+            if self.device >= 0:
+                begining_blanks = begining_blanks.cuda(self.device)
             queryseq_mask_padded   = torch.cat((begining_blanks, res), 1)        # (B, 2L+1)
         else:
             queryseq_mask_padded   = None
         return queryseq_padded, queryseq_mask_padded
 
-    @staticmethod
-    def _class_batch_to_labeling_batch(queryseq_padded, scorematrix, scorematrix_mask=None):
+    def _class_batch_to_labeling_batch(self, queryseq_padded, scorematrix, scorematrix_mask=None):
         """
         Convert dimension 'class' of scorematrix to 'label'
         :param queryseq_padded: (2L+1, B)   -> (B, 2L+1)     LongTensor
@@ -154,8 +166,7 @@ class CTC_Log(object):
         res  = scorematrix.gather(2, queryseq_padded.unsqueeze(1).expand(B, T, L2)) # (B, T, 2L+1), indexing each row of scorematrix with queryseq_padded
         return res
 
-    @staticmethod
-    def _recurrence_relation(queryseq_padded, queryseq_mask_padded=None, blank_symbol=None):
+    def _recurrence_relation(self, queryseq_padded, queryseq_mask_padded=None, blank_symbol=None):
         """
         Generate structured matrix r2 & r3 for dynamic programming recurrence
         :param queryseq_padded: (2L+1, B)         (B, 2L+1)          LongTensor
@@ -165,13 +176,97 @@ class CTC_Log(object):
         """
         B, L2 = queryseq_padded.size()                                                      # = 2L+1
         blanks = Variable(torch.zeros(queryseq_padded.size(0), 2).long() + blank_symbol)                     # (B, 2)
+        if self.device >= 0:
+            blanks = blanks.cuda(self.device)
         ybb = torch.cat((queryseq_padded, blanks), 1)                                       # (B, 2L+3)
         sec_diag = (1 - torch.eq(ybb[:, :-2], ybb[:, 2:])) * torch.eq(ybb[:, 1:-1], blank_symbol)  # (B, 2L+1)
         if queryseq_mask_padded is not None:
             sec_diag = sec_diag * queryseq_mask_padded
         r2 = Variable(torch.Tensor(np.eye(L2, L2, 1)))                                                # upper diagonal matrix (2L+1, 2L+1)
-        r3 = Variable(torch.Tensor(np.eye(L2, L2, 2)).unsqueeze(2).expand(L2, L2, B)) * sec_diag.float().transpose(0,1).unsqueeze(1).expand(L2,L2,B)   # until [3-17-2017], pytorch does not support dimension broadcast, use expand here
+        r3 = Variable(torch.Tensor(np.eye(L2, L2, 2)).unsqueeze(2).expand(L2, L2, B))
+        if self.device >= 0:
+            r2 = r2.cuda(self.device)
+            r3 = r3.cuda(self.device)
+        r3 = r3 * sec_diag.float().transpose(0,1).unsqueeze(1).expand(L2,L2,B)   # until [3-17-2017], pytorch does not support dimension broadcast, use expand here
         return r2, r3  # (2L+1, 2L+1, B)
+
+    @staticmethod
+    def best_path_decode(scorematrix, scorematrix_mask=None, blank_symbol=None):
+        """
+        Computes the best path by simply choosing most likely label at each timestep
+        :param scorematrix: (B, T, C+1)         FloatTensor
+        :param scorematrix_mask: (B, T)         ByteTensor
+        :param blank_symbol: = C by default     Integer
+        :return: resultseq (B, T)               LongTensor
+                 resultseq_mask(B, T)           ByteTensor
+        """
+        B, T, C1          = scorematrix.size()
+        if blank_symbol is None:
+            blank_symbol = C1 - 1
+        bestp, bestlabels = scorematrix.max(2)      # (B, T, 1)
+        resultseq         = bestlabels.squeeze()
+        if scorematrix_mask is None:
+            resultseq_mask = scorematrix.new(B, T).type(torch.ByteTensor).zero_() + 1
+        else:
+            resultseq_mask    = scorematrix_mask.clone()
+
+        for i in range(B):
+            for j in range(T):
+                if resultseq[i, j] == blank_symbol:
+                    resultseq_mask[i, j] = 0
+                    continue
+                if j!=0 and resultseq[i, j] == resultseq[i, j-1]:
+                    resultseq_mask[i, j] = 0
+                    continue
+        return resultseq, resultseq_mask
+
+    @staticmethod
+    def _editdist(s, t):
+        """
+        :param s, t : both LongTensor with size = (L,)
+        :return: integer
+        """
+        ns, nt = s.numel(), t.numel()
+        if s.numel() == 0:
+            return nt
+        elif t.numel() == 0:
+            return ns
+        v0 = s.new(range(nt+1))
+        v1 = s.new(nt+1).zero_()
+        for i in range(ns):
+            v1[0] = i + 1
+            for j in range(nt):
+                cost = 0 if s[i] == t[j] else 1
+                v1[j + 1] = min(v1[j] + 1, v0[j + 1] + 1, v0[j] + cost)
+            for j in range(nt+1):
+                v0[j] = v1[j]
+        return v1[t.numel()]
+
+    def calc_CER(self, resultseq, targetseq, resultseq_mask=None, targetseq_mask=None):
+        """
+         Calculate the character error rate (CER) given ground truth 'targetseq' and CTC decoding output 'resultseq'
+         :param resultseq (B, T1)               LongTensor
+         :param resultseq_mask (B, T1)          ByteTensor
+         :param targetseq (B, T2)               LongTensor
+         :param targetseq_mask (B, T2)          ByteTensor
+         :return: (CER, Total_Error_Len, Total_GT_Len)
+         """
+        assert(resultseq.size(0) == targetseq.size(0))
+        B = resultseq.size(0)
+        total_gt_len, total_ed = 0, 0
+        for i in range(B):
+            s = resultseq[i,:]
+            if resultseq_mask is not None:
+                s = s.masked_select(resultseq_mask[i,:])
+            t = targetseq[i,:]
+            if targetseq_mask is not None:
+                t = t.masked_select(targetseq_mask[i,:])
+            ed = self._editdist(s, t)
+            total_ed += ed
+            total_gt_len += t.numel()
+        CER = total_ed / total_gt_len * 100.0
+        return CER, total_ed, total_gt_len
+
 
 if __name__ == '__main__':
     import numpy as np, time
@@ -227,3 +322,5 @@ if __name__ == '__main__':
 
     result_torch.backward()
     print(sm_v.grad)
+
+
