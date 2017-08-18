a5d1cb56106175de6e5bfd438e43918a498ce4fd
DavidLeon <daweileng@live.com>
2017-08-18 09:14:19 +0800

    add `set_device()` function

--- pytorch_ext/utils/utils.py
+++ pytorch_ext/utils/utils.py
@@ -17,6 +17,20 @@ def get_device(x):
         return -1
     else:
         return x.get_device()
+def set_device(x, device):
+    """
+    Convenient & unified function for moving a Tensor or Variable to a specified device
+    :param x: torch Tensor or Variable
+    :param device: device ID, -1 for CPU; 0, 1, ... for GPU
+    :return: Tensor or Variable on the specified device
+    """
+    if device < 0:
+        if x.is_cuda:
+            return x.cpu()
+        else:
+            return x
+    else:
+        return x.cuda(device)
 
 def grad_clip(parameters, min, max):
     parameters = list(filter(lambda p: p.grad is not None, parameters))
