9424cb67bb46d17c387b4b3ec6a2e2522954173f
David Leon <daweileng@live.com>
2020-09-11 14:33:18 +0800

    remove obsolete set_device() function; rewrite get_device() function

--- pytorch_ext/__init__.py
+++ pytorch_ext/__init__.py
@@ -3,5 +3,5 @@ from . import util
 from . import objective
 from . import functional
 
-__version__ = "0.8.12"
+__version__ = "0.8.13"
 __author__  = "David Leon (Dawei Leng)"
--- pytorch_ext/util.py
+++ pytorch_ext/util.py
@@ -62,30 +62,22 @@ def set_value(t, v):
 
 def get_device(x):
     """
-    Convenient & unified function for getting device a Tensor or Variable resides on
-    :param x: torch Tensor or Variable
-    :return: device ID, -1 for CPU; 0, 1, ... for GPU
+    :param x: Tensor or nn.Module
+    :return: torch.device
     """
-    if x.is_cuda is False:
-        return -1
-    else:
-        return x.get_device()
+    if isinstance(x, torch.Tensor):
+        return x.device
+    elif isinstance(x, torch.nn.Module):
+        device = None
+        for p in x.parameters():
+            device = p.device
+            break
+        if device is None:
+            for p in x.buffers():
+                device = p.device
+                break
+        return device
 		
-def set_device(x, device):
-    """
-    Convenient & unified function for moving a Tensor or Variable to a specified device
-    :param x: torch Tensor or Variable
-    :param device: device ID, -1 for CPU; 0, 1, ... for GPU
-    :return: Tensor or Variable on the specified device
-    """
-    if device < 0:
-        if x.is_cuda:
-            return x.cpu()
-        else:
-            return x
-    else:
-        return x.cuda(device)
-
 def grad_clip(parameters, min, max):
     parameters = list(filter(lambda p: p.grad is not None, parameters))
     for p in parameters:
